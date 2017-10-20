b9ae5fc2c529be6ec642b6e139d32f338b300c12
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-10-20 12:59:22 +0300

    Fix "/files" route bug

--- server-nodejs/utils/id.js
+++ server-nodejs/utils/id.js
@@ -3,14 +3,14 @@
 let base64url = require('base64url');
 
 function encode(path) {
-  let result = encodeURIComponent(base64url(path));
+  let result = base64url(path);
   console.log('path:', path);
   console.log('encoded:', result);
   return result;
 };
 
 function decode(id) {
-  let result = decodeURIComponent(base64url.decode(id));
+  let result = base64url.decode(id);
   console.log('id:', id);
   console.log('decoded:', result);
   return result;
