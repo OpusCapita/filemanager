3026d2aa95d40293d679c28350d4e27edd21c158
Andrei Mourzenkov <mourzenkov@scand.com>
2017-10-26 22:21:30 +0300

    POST api/files - multiple/single files upload and dir creation

--- server-nodejs/routes/files.js
+++ server-nodejs/routes/files.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const fs = require('fs-extra');
+const multer = require('multer')
 
 const getClientIp = require('../utils/get-client-ip');
 
@@ -13,6 +14,31 @@ const {
 const UNKNOWN_RESOURCE_TYPE_ERROR = 'Unknown resource type';
 
 module.exports = (app, options) => {
+  const upload = multer({
+    storage: multer.diskStorage({
+      destination(req, file, cb) {
+        const { title, parentId, type } = req.body;
+
+        if (type !== file) {
+          cb(new Error('type ${type} conflicts with files field requested by ${getClientIp(req)}'));
+          return;
+        }
+
+        if (title) {
+          cb(new Error('title ${title} conflicts with files field requested by ${getClientIp(req)}'));
+          return;
+        }
+
+        const reqParentPath = id2path(parentId);
+        const absPath = path.resolve(options.fsRoot, '.' + reqParentPath.split('/').join(path.sep));
+        cb(null, absPath);
+      },
+      filename(req, file, cb) {
+        cb(null, file.originalname);
+      }
+    })
+  });
+
   const stat2resource = pathInfo => stat => {
     let userPath, userParent, userBasename;
 
@@ -49,23 +75,22 @@ module.exports = (app, options) => {
       throw new Error(UNKNOWN_RESOURCE_TYPE_ERROR);
     }
 
-    console.log('*** stat2resource returns', resource);
     return resource;
   }
 
   app.get('/api/files/:id/children', (req, res) => {
     const id = req.params.id;
-    const requestedPath = id2path(id);
-    const absolutePath = path.resolve(options.fsRoot, '.' + requestedPath.split('/').join(path.sep));
-    options.logger.info(`Children for ${absolutePath} requested by ${getClientIp(req)}`);
+    const reqPath = id2path(id);
+    const absPath = path.resolve(options.fsRoot, '.' + reqPath.split('/').join(path.sep));
+    options.logger.info(`Children for ${absPath} requested by ${getClientIp(req)}`);
 
-    fs.readdir(absolutePath).
+    fs.readdir(absPath).
       then(basenames =>
-        Promise.all(basenames.map(basename => fs.stat(absolutePath + path.sep + basename))).
+        Promise.all(basenames.map(basename => fs.stat(absPath + path.sep + basename))).
           then(stats => stats.reduce(
             (rez, stat, i) => {
               try {
-                rez.push(stat2resource({ dir: requestedPath, basename: basenames[i] })(stat));
+                rez.push(stat2resource({ dir: reqPath, basename: basenames[i] })(stat));
               } catch (err) {
                 if (err.message !== UNKNOWN_RESOURCE_TYPE_ERROR) {
                   throw err;
@@ -76,11 +101,7 @@ module.exports = (app, options) => {
             },
             []
           )).
-          then(rez => res.json(rez)).
-          catch(err => {
-            options.logger.error(err);
-            res.status(204).end();
-          })
+          then(rez => res.json(rez))
       ).
       catch(err => {
         options.logger.error(err);
@@ -90,12 +111,12 @@ module.exports = (app, options) => {
 
   app.get('/api/files/:id', (req, res) => {
     const id = req.params.id;
-    const requestedPath = id2path(id);
-    const absolutePath = path.resolve(options.fsRoot, '.' + requestedPath.split('/').join(path.sep));
-    options.logger.info(`Stat for ${absolutePath} requested by ${getClientIp(req)}`);
+    const reqPath = id2path(id);
+    const absPath = path.resolve(options.fsRoot, '.' + reqPath.split('/').join(path.sep));
+    options.logger.info(`Stat for ${absPath} requested by ${getClientIp(req)}`);
 
-    fs.stat(absolutePath).
-      then(stat2resource(requestedPath)).
+    fs.stat(absPath).
+      then(stat2resource(reqPath)).
       then(rez => res.json(rez)).
       catch(err => {
         options.logger.error(err);
@@ -104,15 +125,84 @@ module.exports = (app, options) => {
   });
 
   app.get('/api/files', (req, res) => {
-    const absolutePath = path.resolve(options.fsRoot);
-    options.logger.info(`Stat for ${absolutePath} requested by ${getClientIp(req)}`);
+    const absPath = path.resolve(options.fsRoot);
+    options.logger.info(`Stat for ${absPath} requested by ${getClientIp(req)}`);
 
-    fs.stat(absolutePath).
+    fs.stat(absPath).
       then(stat2resource('/')).
       then(rez => res.json(rez)).
       catch(err => {
         options.logger.error(err);
         res.status(204).end();
       });
+  });
+
+  app.delete('/api/files/:id', (req, res) => {
+    const id = req.params.id;
+    const reqPath = id2path(id);
+    const absPath = path.resolve(options.fsRoot, '.' + reqPath.split('/').join(path.sep));
+    options.logger.info(`Delete ${absPath} requested by ${getClientIp(req)}`);
+
+    fs.remove(absPath).
+      then(_ => res.status(200).end()).
+      catch(err => {
+        options.logger.error(err);
+        res.status(204).end();
+      });
+  });
+
+  app.post('/api/files', upload.array('files'), (req, res) => {
+    const { title, parentId, type } = req.body;
+
+    if (type === 'dir') {
+      if (!title) {
+        options.logger.error(`ERROR: unable to create empty title requested by ${getClientIp(req)}`);
+        res.status(204).end();
+        return;
+      }
+
+      if (~title.indexOf(path.sep)) {
+        options.logger.error(`ERROR: unable to create title with forbidden symbols requested by ${getClientIp(req)}`);
+        res.status(204).end();
+        return;
+      }
+
+      const reqParentPath = id2path(parentId);
+      const absPath = path.resolve(options.fsRoot, '.' + reqParentPath.split('/').join(path.sep) + path.sep + title);
+      options.logger.info(`Create dir ${absPath} requested by ${getClientIp(req)}`);
+
+      // fs.access makes sure that parent exists.
+      fs.access(path.resolve(options.fsRoot, '.' + reqParentPath.split('/').join(path.sep))).
+        then(_ => fs.ensureDir(absPath)).
+        then(_ => fs.stat(absPath)).
+        then(stat2resource({ dir: reqParentPath, basename: title })).
+        then(rez => res.json(rez)).
+        catch(err => {
+          options.logger.error(err);
+          res.status(204).end();
+        });
+    } else if (type === 'file') {
+      if (title) {
+        options.logger.error(`ERROR: title ${title} conflicts with type requested by ${getClientIp(req)}`);
+        res.status(204).end();
+        return;
+      }
+
+      Promise.all(req.files.map(({ path, filename }) =>
+        fs.stat(path).
+          then(stat2resource({
+            dir: parentId,
+            basename: filename
+          }))
+      )).
+        then(rez => res.json(rez)).
+        catch(err => {
+          options.logger.error(err);
+          res.status(204).end();
+        })
+    } else {
+      options.logger.error(`ERROR: unable to create title of unknown type ${type} requested by ${getClientIp(req)}`);
+      res.status(204).end();
+    }
   });
 }
--- server-nodejs/utils/id.js
+++ server-nodejs/utils/id.js
@@ -4,17 +4,17 @@ const base64url = require('base64url');
 
 function encode(path) {
   const result = base64url(path);
-  console.log('path:', path);
-  console.log('encoded:', result);
+  console.log('■■■ path:', path);
+  console.log('■■■ encoded:', result);
   return result;
-};
+}
 
 function decode(id) {
   const result = base64url.decode(id);
-  console.log('id:', id);
-  console.log('decoded:', result);
+  console.log('■■■ id:', id);
+  console.log('■■■ decoded:', result);
   return result;
-};
+}
 
 module.exports = {
   encode,
