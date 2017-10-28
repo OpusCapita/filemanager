670e4132a2802dcbc7ecf0d8d23eaa29b4b1b0e9
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-10-28 22:23:37 +0300

    Add more icons for different file types

--- client-react/src/client/components/ListView/icons.js
+++ client-react/src/client/components/ListView/icons.js
@@ -2,12 +2,14 @@ let dirIcon = require('!!raw-loader!@opuscapita/svg-icons/lib/folder.svg');
 let soundFileIcon = require('!!raw-loader!@opuscapita/svg-icons/lib/audiotrack.svg');
 let pictureFileIcon = require('!!raw-loader!@opuscapita/svg-icons/lib/image.svg');
 let videoFileIcon = require('!!raw-loader!@opuscapita/svg-icons/lib/ondemand_video.svg');
+let archiveFileIcon = require('!!raw-loader!@opuscapita/svg-icons/lib/archive.svg');
 let unknownFileIcon = require('!!raw-loader!@opuscapita/svg-icons/lib/insert_drive_file.svg');
 
 let defaultFillColor = '#333';
 let soundFilesExtensions = ['aac', 'aiff', 'flac', 'm4a', 'ogg', 'mp3', 'wav', 'wma'];
 let pictureFilesExtensions = ['gif', 'png', 'jpg', 'jpeg', 'bmp', 'svg'];
 let videoFilesExtensions = ['avi', 'flv', 'wmv', 'mov', 'mp4'];
+let archiveFilesExtensions = ['tar.gz', 'zip', 'gz'];
 
 function matchFileExtensions(filename, extensions) {
   let extensionsRegExp = `(${extensions.join('|')})`;
@@ -23,6 +25,8 @@ export function getIcon(resource) {
     return { svg: pictureFileIcon, fill: defaultFillColor };
   } else if (matchFileExtensions(resource.title, videoFilesExtensions)) {
     return { svg: videoFileIcon, fill: defaultFillColor };
+  } else if (matchFileExtensions(resource.title, archiveFilesExtensions)) {
+    return { svg: archiveFileIcon, fill: defaultFillColor };
   } else {
     return { svg: unknownFileIcon, fill: defaultFillColor };
   }
