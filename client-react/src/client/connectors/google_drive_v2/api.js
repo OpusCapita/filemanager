0763c92167028c94b2d1db0a88e1c316488b3483
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-10-30 22:37:05 +0300

    Remove unused code

--- client-react/src/client/connectors/google_drive_v2/api.js
+++ client-react/src/client/connectors/google_drive_v2/api.js
@@ -1,43 +1,4 @@
-import id from '../../../../../server-nodejs/utils/id';
-
 let signedIn = false;
-
-let listViewLayout = (renderOptions) => ([
-  {
-    width: 48,
-    label: 'Title',
-    dataKey: 'title',
-    flexGrow: 1,
-    cellRenderer: NameCell(renderOptions),
-    headerRenderer: HeaderCell(renderOptions),
-    disableSort: false,
-    hidden: false
-  },
-  {
-    width: 100,
-    dataKey: 'size',
-    label: 'File size',
-    flexGrow: renderOptions.clientWidth > TABLET_WIDTH ? 1 : 0,
-    cellRenderer: SizeCell(renderOptions),
-    headerRenderer: HeaderCell(renderOptions),
-    disableSort: true,
-    hidden: false
-  },
-  {
-    width: 100,
-    dataKey: 'modifyDate',
-    label: 'Last modified',
-    flexGrow: 1,
-    cellRenderer: DateTimeCell(renderOptions),
-    headerRenderer: HeaderCell(renderOptions),
-    disableSort: true,
-    hidden: renderOptions.clientWidth < MOBILE_WIDTH
-  }
-]);
-
-let initiallSortBy = 'title';
-let initiallSortDirection = 'ASC';
-/* RENDER DEFINITION END */
 
 function appendGoogleApiScript() {
   if (window.gapi) {
