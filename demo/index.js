138184230ac544b3eb815d1b28ef76b802e83d7a
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-11-01 12:43:16 +0300

    Fix demo

--- demo/index.js
+++ demo/index.js
@@ -21,7 +21,7 @@ fs.writeFileSync(
   'window.env = ' + JSON.stringify(env) + ';'
 );
 
-app.use(filemanagerMiddleware(app, config));
+app.use(filemanagerMiddleware(config));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
