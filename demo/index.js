ca3387ce6275562f8ee6fe18e7cfe91de6c1c1dd
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-11-13 00:34:45 +0300

    Fix demo bug

--- demo/index.js
+++ demo/index.js
@@ -7,7 +7,7 @@ const env = require('./.env');
 
 const config = {
   fsRoot: path.resolve(__dirname, './demo-fs'),
-  rootTitle: 'Customization area',
+  rootName: 'Customization area',
   port: process.env.PORT || '3020',
   host: process.env.HOST || 'localhost'
 };
