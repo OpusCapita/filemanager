564a4ef79311c78e0730f0569c41585a638cdb8c
Andrei Mourzenkov <mourzenkov@scand.com>
2017-11-14 17:55:21 +0300

    Preview puts correct filename in 'Save As...' dialog for unknown mime types

--- server-nodejs/router/download.js
+++ server-nodejs/router/download.js
@@ -6,6 +6,19 @@ const fs = require('fs-extra');
 const getClientIp = require('../utils/get-client-ip');
 const { id2path } = require('./lib');
 
+/*
+ * The function is copied from
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent#Examples
+ * See also for more details
+ * https://stackoverflow.com/questions/11598274/display-pdf-in-browser-using-express-js
+ */
+const encodeRFC5987ValueChars = str => encodeURIComponent(str).
+  // Note that although RFC3986 reserves "!", RFC5987 does not, so we do not need to escape it
+  replace(/['()*]/g, c => "%" + c.charCodeAt(0).toString(16)). // i.e., %27 %28 %29 %2A
+  // The following are not required for percent-encoding per RFC5987,
+  // so we can allow for a little better readability over the wire: |`^
+  replace(/%(7C|60|5E)/g, (str, hex) => String.fromCharCode(parseInt(hex, 16)));
+
 module.exports = ({ options, req, res, handleError }) => {
   const ids = Array.isArray(req.query.items) ? req.query.items : [req.query.items];
   const preview = req.query.preview === 'true';
@@ -36,8 +49,11 @@ module.exports = ({ options, req, res, handleError }) => {
           });
         } else if (preview) {
           // Sets the Content-Type response HTTP header field based on the filename’s extension.
-          // The Content-Disposition is not set, i.e. "inline" parameter is the default for a browser.
-          res.sendFile(absPaths[0]);
+          res.sendFile(absPaths[0], {
+            headers: {
+              'Content-Disposition': `inline; filename="${encodeRFC5987ValueChars(path.basename(absPaths[0]))}"`
+            }
+          });
         } else {
           // Sets the Content-Type response HTTP header field based on the filename’s extension.
           // Sets the Content-Disposition with "attachment" and "filename" equal to path.basename(absPaths[0])
