cfe5068d5cd785339a3165f3b329b89d2a9986ce
Kirill Volkovich <kvolkovich-sc@users.noreply.github.com>
2017-11-20 01:42:06 +0300

    Use "lerna" as a main build tool (#107)
    * Init "lerna"
    
    * Remove "utils" dir
    
    * Add "chokidar" for watching files
    
    * Update "lerna" config
    
    * Update "lerna" config
    
    * Update "lerna" config
    
    * Update "lerna" config
    
    * Update "lerna" config
    
    * v1.0.0-beta.2
    
    * Update "server-nodejs" publish config
    
    * v1.0.0-beta.3

--- packages/server-nodejs/config/server-config.js
+++ packages/server-nodejs/config/server-config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 module.exports = {
-  fsRoot: path.resolve('../demo/demo-fs'),
+  fsRoot: path.resolve('../../demo/demo-fs'),
   rootName: 'Customization area',
   port: process.env.PORT || '3020',
   host: process.env.HOST || 'localhost'
