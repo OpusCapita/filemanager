7f670ab79b4bd3cc291cc4f5a8fb8d09ad7dbd78
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2017-12-06 21:22:47 +0300

    #80 Success read vault

--- .circleci/scripts/env.js
+++ .circleci/scripts/env.js
@@ -4,8 +4,6 @@ let env = process.env;
 let DOCKER_TAG = env.CIRCLE_BRANCH.replace('/', '-').replace('#', '-');
 
 console.log('--- env.GH_MAIL', env.GH_MAIL);
-console.log('whoami', require('./run')('whoami'));
-console.log('shell', require('./run')('env | grep SHELL'));
 
 module.exports = {
   AZURE_APP_NAME: "opuscapita-filemanager-demo",
