c5e1eec65a4b9c663d92f263ee3e637bb855be62
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2017-12-06 22:33:31 +0300

    #80 Test build

--- .circleci/scripts/add-github-pr-message.js
+++ .circleci/scripts/add-github-pr-message.js
@@ -14,14 +14,16 @@ let targets = pullRequests.map(pr => pr.head.sha);
 console.log('--- Add status message to these commits:', JSON.stringify(targets, null, 4));
 
 
+let demoUrl = "http://opuscapita-filemanager-demo-${env.DOCKER_TAG}.azurewebsites.net";
+
 targets.forEach(target => {
   run(`
     http --ignore-stdin --auth ${env.GH_NAME}:${env.GH_PASS} \
     POST \
     https://api.github.com/repos/${env.PROJECT_USERNAME}/${env.PROJECT_REPONAME}/statuses/${target} \
     state="success" \
-    target_url="http://opuscapita-filemanager-demo-${env.DOCKER_TAG}.azurewebsites.net" \
-    description="Test status" \
+    target_url="${demoUrl}" \
+    description="Demo available at: ${demoUrl}" \
     context="ci/demo-deploy"
   `);
 });
