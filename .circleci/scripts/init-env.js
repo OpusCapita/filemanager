008d66a677400a7f8865dcc0d29554151ee2e497
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2017-12-06 21:21:51 +0300

    #80 Test build

--- .circleci/scripts/init-env.js
+++ .circleci/scripts/init-env.js
@@ -12,4 +12,4 @@ run(`
   openssl aes-256-cbc -d -in .secrets -out .secrets-plain -k ${process.env.MACHINEUSER_VAULT_KEY}
 `);
 
-run(`echo "source /vault/.secrets-plain" >> ~/.bashrc`);
+run(`echo "source /vault/.secrets-plain" >> ${process.env.CIRCLE_SHELL_ENV}`);
