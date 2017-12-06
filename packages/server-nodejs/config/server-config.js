7288133740926f776e87cbae35e0699fa1a25043
Andrei Mourzenkov <mourzenkov@scand.com>
2017-12-06 12:00:48 +0300

    Resolve #120

--- packages/server-nodejs/config/server-config.js
+++ packages/server-nodejs/config/server-config.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = {
   fsRoot: path.resolve('../../demo/demo-fs'),
   rootName: 'Customization area',
+  readOnly: false,
   port: process.env.PORT || '3020',
   host: process.env.HOST || 'localhost'
 };
