6ba795f9a08471d7b01e71239fb0806aa5241c5c
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-12-22 16:02:33 +0300

    Fix missing headers of 'server-nodejs'

--- packages/server-nodejs/router/index.js
+++ packages/server-nodejs/router/index.js
@@ -16,6 +16,7 @@ module.exports = options => {
 
   router.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,POST,HEAD,OPTIONS,PUT,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
   router.use(helmet.noCache());
--- packages/server-nodejs/server.js
+++ packages/server-nodejs/server.js
@@ -10,7 +10,6 @@ function run(config = require('./config/server-config')) {
 
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
