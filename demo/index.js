47c1cf0337e983f660e5374f98ce324ab3ce7ebb
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-12-22 17:23:19 +0300

    Remove more unnecessary code

--- demo/index.js
+++ demo/index.js
@@ -21,7 +21,6 @@ fs.writeFileSync(
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
