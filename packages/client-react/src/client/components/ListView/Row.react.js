8e5fbbf028525c54f7e02276c00afb5660a02b30
Egor Stambakio <31243790+estambakio-sc@users.noreply.github.com>
2018-01-29 11:14:55 +0300

    (#131) Moved ListView keyboard listeners to a separate HOC (#134)

--- packages/client-react/src/client/components/ListView/Row.react.js
+++ packages/client-react/src/client/components/ListView/Row.react.js
@@ -115,7 +115,8 @@ class Row extends Component {
             `}
             key={rowData.id}
             role="row"
-            style={style}>
+            style={style}
+          >
             {columns}
           </div>
         )))}
