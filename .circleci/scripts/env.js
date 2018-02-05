26039cddb2eda4a110482c928a6d3c1eeb8cb614
Kirill Volkovich <kvolkovich-sc@users.noreply.github.com>
2018-02-05 17:52:49 +0300

    Update env.js

--- .circleci/scripts/env.js
+++ .circleci/scripts/env.js
@@ -4,17 +4,6 @@ let run = require('./run');
 let env = process.env;
 let DOCKER_TAG = env.CIRCLE_BRANCH.replace('/', '-').replace('#', '-');
 
-let shouldDeployDemo = () => {
-  let gitCommitMessage = run(`git log --format=oneline -n 1 ${env.CIRCLE_SHA1}`);
-
-  return (
-    gitCommitMessage.includes('[ci deploy demo]') ||
-    gitCommitMessage.includes('[ci demo deploy]') ||
-    gitCommitMessage.includes('[deploy demo ci]') ||
-    gitCommitMessage.includes('[demo deploy ci]')
-  );
-}
-
 module.exports = {
   AZURE_APP_NAME: "opuscapita-filemanager-demo",
   AZURE_APP_SERVICE_PLAN: env.MINSK_CORE_DEMO_AZURE_APP_SERVICE_PLAN,
@@ -35,6 +24,5 @@ module.exports = {
   API_KEY: env.MINSK_CORE_DEMO_GOOGLE_DRIVE_API_KEY,
   CLIENT_ID: env.MINSK_CORE_DEMO_GOOGLE_DRIVE_CLIENT_ID,
   CHANGELOG_GITHUB_TOKEN: env.MINSK_CORE_DEMO_CHANGELOG_GITHUB_TOKEN,
-  SERVER_URL: `http://opuscapita-filemanager-demo-${DOCKER_TAG}.azurewebsites.net`,
-  SHOULD_DEPLOY_DEMO: shouldDeployDemo() ? 'true' : 'false'
+  SERVER_URL: `http://opuscapita-filemanager-demo-${DOCKER_TAG}.azurewebsites.net`
 };
