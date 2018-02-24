47f084b77fb714856696def4872cfa479d54aa0d
parhamzare <parhamzare1@gmail.com>
2018-02-24 15:23:20 +0330

    addCustomHeader

--- packages/connector-node-v1/src/api.js
+++ packages/connector-node-v1/src/api.js
@@ -32,9 +32,15 @@ async function getCapabilitiesForResource(options, resource) {
 async function getResourceById(options, id) {
   let route = `${options.apiRoot}/files/${id}`;
   let method = 'GET';
-  let response = await request(method, route).catch((error) => {
-    console.error(`Filemanager. getResourceById(${id})`, error);
-  });
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
+  let response = await request(method, route)
+    .set(header)
+    .catch((error) => {
+      console.error(`Filemanager. getResourceById(${id})`, error);
+    });
 
   let resource = response.body;
 
@@ -44,12 +50,18 @@ async function getResourceById(options, id) {
 async function getChildrenForId(options, { id, sortBy = 'name', sortDirection = 'ASC', onFail }) {
   let route = `${options.apiRoot}/files/${id}/children?orderBy=${sortBy}&orderDirection=${sortDirection}`;
   let method = 'GET';
-  let response = await request(method, route).catch((error) => {
-    console.error(`Filemanager. getChildrenForId(${id})`, error);
-    if (onFail) {
-      onFail({ message: getMessage(options.locale, 'unableReadDir') }) // TODO doesn't intercept for some reason
-    }
-  });
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
+  let response = await request(method, route)
+    .set(header)
+    .catch((error) => {
+      console.error(`Filemanager. getChildrenForId(${id})`, error);
+      if (onFail) {
+        onFail({ message: getMessage(options.locale, 'unableReadDir') }) // TODO doesn't intercept for some reason
+      }
+    });
 
   let rawResourceChildren = response.body.items;
   let resourceChildren = await Promise.all(rawResourceChildren.map(async (o) => normalizeResource(o)));
@@ -79,9 +91,15 @@ async function getParentsForId(options, id, result = []) {
 
 async function getBaseResource(options) {
   let route = `${options.apiRoot}/files`;
-  let response = await request.get(route).catch((error) => {
-    console.error('Filemanager. getBaseResource()', error);
-  });
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
+  let response = await request.get(route)
+    .set(header)
+    .catch((error) => {
+      console.error('Filemanager. getBaseResource()', error);
+    });
   return normalizeResource(response.body);
 }
 
@@ -150,31 +168,40 @@ async function readLocalFile() {
 async function uploadFileToId(options, parentId, { onStart, onSuccess, onFail, onProgress }) {
   let file = await readLocalFile(true);
   let route = `${options.apiRoot}/files`;
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
   onStart({ name: file.name, size: file.file.size });
-  request.post(route).
-  field('type', 'file').
-  field('parentId', parentId).
-  attach('files', file.file, file.name).
-  on('progress', event => {
-    onProgress(event.percent);
-  }).
-  end((error, response) => {
-    if (error) {
-      console.log(`Filemanager. uploadFileToId(${parentId})`, error);
-      onFail();
-    } else {
-      let newResource = normalizeResource(response.body[0]);
-      onSuccess(newResource.id);
-    }
-  });
+  request.post(route)
+    .set(header)
+    .field('type', 'file').
+    field('parentId', parentId).
+    attach('files', file.file, file.name).
+    on('progress', event => {
+      onProgress(event.percent);
+    }).
+    end((error, response) => {
+      if (error) {
+        console.log(`Filemanager. uploadFileToId(${parentId})`, error);
+        onFail();
+      } else {
+        let newResource = normalizeResource(response.body[0]);
+        onSuccess(newResource.id);
+      }
+    });
 }
 
 async function downloadResource({ apiOptions, resource, onProgress, i, l, onFail }) {
   const downloadUrl = `${apiOptions.apiRoot}/download?items=${resource.id}`;
-
-  return request.get(downloadUrl).
-    responseType('blob').
-    on('progress', event => {
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
+  return request.get(downloadUrl)
+    .set(header)
+    .responseType('blob')
+    .on('progress', event => {
       onProgress((i * 100 + event.percent) / l);
     }).
     then(
@@ -186,7 +213,7 @@ async function downloadResource({ apiOptions, resource, onProgress, i, l, onFail
         console.error(err);
         onFail();
       }
-  );
+    );
 }
 
 async function downloadResources({ apiOptions, resources, trackers: {
@@ -236,16 +263,22 @@ async function downloadResources({ apiOptions, resources, trackers: {
 async function createFolder(options, parentId, folderName, { onFail }) {
   let route = `${options.apiRoot}/files`;
   let method = 'POST';
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
   let params = {
     parentId,
     name: folderName,
     type: 'dir'
   };
-  let response = await request(method, route).send(params).
-  catch((error) => {
-    console.error(`Filemanager. createFolder(${id})`, error);
-    onFail()
-  });
+  let response = await request(method, route)
+    .send(params)
+    .set(header).
+    catch((error) => {
+      console.error(`Filemanager. createFolder(${id})`, error);
+      onFail()
+    });
   return response;
 }
 
@@ -256,30 +289,40 @@ function getResourceName(apiOptions, resource) {
 async function renameResource(options, id, newName, { onFail }) {
   let route = `${options.apiRoot}/files/${id}`;
   let method = 'PATCH';
-  let response = await request(method, route).type('application/json').send({ name: newName }).
-  catch((error) => {
-    console.error(`Filemanager. renameResource(${id})`, error);
-    onFail()
-  });
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
+  let response = await request(method, route)
+    .set(header)
+    .type('application/json').send({ name: newName }).
+    catch((error) => {
+      console.error(`Filemanager. renameResource(${id})`, error);
+      onFail()
+    });
   return response;
 }
 
 async function removeResource(options, resource) {
   let route = `${options.apiRoot}/files/${resource.id}`;
   let method = 'DELETE';
-  let response = await request(method, route).
-  catch((error) => {
-    throw error;
-  });
+  let header = { 'Content-Type': 'application/json' };
+  if (options.apiToken != undefined) {
+    header['Authorization'] = options.apiToken;
+  }
+  let response = await request(method, route).set(header).
+    catch((error) => {
+      throw error;
+    });
   return response;
 }
 
 async function removeResources(options, selectedResources, { onSuccess, onFail }) {
   let success = await Promise.all(selectedResources.map(async (resource) => await removeResource(options, resource))).
-  catch((error) => {
-    console.error(`Filemanager. removeResources`, error);
-    onFail();
-  });
+    catch((error) => {
+      console.error(`Filemanager. removeResources`, error);
+      onFail();
+    });
   onSuccess();
 }
 
