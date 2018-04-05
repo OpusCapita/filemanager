e8236d20b63a8effdabd718ac1d3b5674e2dc9a4
Aleksandr Baliunov <abaliunov-sc@users.noreply.github.com>
2018-04-05 12:40:12 +0300

    (#194) Stream download of archived files (#195)
    * #194 Stream download of archived files
    
    * #194 Remove old code
    
    * Fix reviewer's note

--- packages/connector-node-v1/src/utils/common.js
+++ packages/connector-node-v1/src/utils/common.js
@@ -1,12 +1,4 @@
 // execute Promises in series
-export const serializePromises = ({ series, onProgress }) => series.reduce(
-  (acc, cur, i) => acc.then(accValues => {
-    onProgress(100 / series.length * i);
-    return Promise.all([...accValues, cur({ onProgress, i, l: series.length })])
-  }),
-  Promise.resolve([])
-)
-
 export function normalizeResource(resource) {
   if (resource) {
     return {
