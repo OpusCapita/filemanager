ac93b9c2698d0dced76c97aa0d522d92aca79480
Egor Stambakio <stambakio@scand.com>
2018-08-17 17:00:25 +0300

    Fixing build process

--- demo/.env.js
+++ demo/.env.js
@@ -3,7 +3,8 @@
 module.exports = {
   HOST: process.env.HOST ? process.env.HOST : 'localhost',
   PORT: process.env.PORT ? process.env.PORT : 3000,
-  SERVER_URL: process.env.SERVER_URL ? process.env.SERVER_URL : 'http://localhost:3020',
+  BASE_URL: process.env.BASE_URL ? process.env.BASE_URL : '',
+  SERVER_URL: process.env.SERVER_URL ? process.env.SERVER_URL : `http://${HOST}:${PORT}${BASE_URL}`,
 
   CLIENT_ID: process.env.CLIENT_ID,
   API_SECRET: process.env.API_SECRET,
