6e13dfe6f1e2d15ee30883bd56fc9f9cc3746368
Alexey Sergeev <sab@scand.com>
2018-09-03 11:25:26 +0300

    Restructuring files and build process

---
+++ demo-filesystem/populate-demo-fs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cp -r $SCRIPT_DIR/files/ $1
+mkdir -p $1/Misc
+
+########    User settings     ############
+MAXDIRS=5
+MAXDEPTH=4
+MAXFILES=20
+MAXSIZE=10
+######## End of user settings ############
+
+# How deep in the file system are we now?
+TOP=`echo "$1/Misc" |tr -cd '/'|wc -c`
+
+populate() {
+	CURDIR=$1
+
+	files=$(($RANDOM*$MAXFILES/32767))
+	for n in `seq $files`
+	do
+		f=`mktemp ${CURDIR}/XXXXXX`
+		size=$(($RANDOM*$MAXSIZE/32767))
+		head -c $size /dev/urandom > $f
+	done
+
+	depth=`echo $CURDIR |tr -cd '/'|wc -c`
+	if [ $(($depth-$TOP)) -ge $MAXDEPTH ]
+	then
+		return
+	fi
+
+  dirs=$(($RANDOM*$MAXDIRS/32767))
+	for n in `seq $dirs`
+	do
+		d=`mktemp -d $CURDIR/XXXXXX`
+	done
+
+  # echo "CURDIR $CURDIR"
+  for dir in `find $CURDIR -maxdepth 1 -mindepth 1 -type d` ; do
+    # echo "dir $dir"
+    populate $dir
+  done
+}
+
+populate $1/Misc
---
+++ packages/demoapp/.env.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const HOST = process.env.HOST ? process.env.HOST : 'localhost';
+const PORT = process.env.PORT ? process.env.PORT : 3000;
+const BASE_URL = process.env.BASE_URL ? process.env.BASE_URL : '';
+const SERVER_HOST = process.env.SERVER_HOST ? process.env.SERVER_HOST : 'localhost';
+const SERVER_URL = process.env.SERVER_URL ?
+  process.env.SERVER_URL :
+  process.env.SERVER_HOST ?
+    `https://${SERVER_HOST}${BASE_URL}` :
+    `http://${HOST}:${PORT}${BASE_URL}`;
+
+
+module.exports = {
+  HOST,
+  PORT,
+  BASE_URL,
+  SERVER_URL,
+
+  CLIENT_ID: process.env.CLIENT_ID,
+  API_KEY: process.env.API_KEY
+};
