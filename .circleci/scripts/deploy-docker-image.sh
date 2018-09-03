9dde8b5007c122770f007b581adffea3163a8eb6
Alexey Sergeev <sab@scand.com>
2018-09-03 22:35:34 +0300

    Reconfiguring build process

--- .circleci/scripts/deploy-docker-image.sh
+++ .circleci/scripts/deploy-docker-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-. .circleci/scripts/common.sh
+. ./common.sh
 
 docker build -f Dockerfile.demo -t ${DOCKER_REPO}:${DOCKER_TAG} .
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
