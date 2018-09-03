bd420707d296dfb5ea96b5deab480dba8d980e5e
Alexey Sergeev <sab@scand.com>
2018-09-03 21:57:29 +0300

    Reconfiguring build process

--- .circleci/scripts/common.sh
+++ .circleci/scripts/common.sh
@@ -4,7 +4,7 @@ slugify() {
   echo $1 | iconv -t ascii//TRANSLIT | sed -E s/[^a-zA-Z0-9]+/-/g | sed -E s/^-+\|-+$//g | tr A-Z a-z | cut -c1-53
 }
 
-DOCKER_REPO=$(echo "$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME" | tr '[:upper:]' '[:lower:]')
+DOCKER_REPO=$(echo "$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME-demo" | tr '[:upper:]' '[:lower:]')
 DOCKER_TAG=$(slugify "${CIRCLE_BRANCH}")
 
 BASE_URL=`echo "/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BRANCH}" | cut -c1-53`
