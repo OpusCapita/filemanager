f0d98f132811af1348a15258d20d23fdacca9c6f
nickvnv <nikolay.ivanov@novexx.com>
2022-03-22 17:14:02 +0100

    if the OS (e.g. Embedded Linux Platform) where File Manager is running, support only one language - usually LANG=en_US(utf8 encoding), then the file names which are ANSI encoded with characters like - ü,ä,ö are not displayed (including all items in the current folder). This fix make them visible, but not processable (rename, delete, etc.).

--- packages/connector-node-v1/src/icons.js
+++ packages/connector-node-v1/src/icons.js
@@ -1,7 +1,8 @@
 import icons from './icons-svg';
 
 const dirIcon = icons.folder;
-const brokenlinkIcon = icons.brokenlink;
+const brokenLinkIcon = icons.brokenLink;
+const encodingNameError = icons.encodingNameError;
 const soundFileIcon = icons.volumeUp;
 const pictureFileIcon = icons.image;
 const videoFileIcon = icons.ondemandVideo;
@@ -25,7 +26,9 @@ export function getIcon(resource) {
   if (resource.type === 'dir') {
     return { svg: dirIcon, fill: defaultFillColor };
   } else if (resource.type === 'brokenlink') {
-    return { svg: brokenlinkIcon, fill: defaultFillColor };
+    return { svg: brokenLinkIcon, fill: defaultFillColor };
+  } else if (resource.type === 'encodingnameerror') {
+    return { svg: encodingNameError, fill: defaultFillColor };
   } else if (matchFileExtensions(resource.name, soundFilesExtensions)) {
     return { svg: soundFileIcon, fill: `#e53935` };
   } else if (matchFileExtensions(resource.name, pictureFilesExtensions)) {
